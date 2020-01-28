242e164d642400217e6f2173308463f95fa1f1c9
glaucio <glauciocardoso@gmail.com>
2020-01-28 13:49:50 -0300

    criar função cadastro pessoa fisica

--- usuarios/views.py
+++ usuarios/views.py
@@ -16,3 +16,15 @@ def cadastro_empresa(request):
         return render(request, 'cadastro_empresa.html', args)
     return render(request, 'cadastro_empresa.html', args)
 
+def cadastrar_pessoa_fisica(request):
+    form = PessoaFisicaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        args = {
+             'form': form, 
+             'msg': 'Cadastro foi realizado com sucesso!'
+        }
+        render(request, 'cadastro.html', args)
+        args = {'form': form}
+        return render('request.cadastro.html', args)
+        args = {form'form'}
