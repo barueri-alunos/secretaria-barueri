a802a03e0ec3b35983359e32cd7db584813161c9
diandra <di.andrade90@gmail.com>
2020-01-30 19:28:40 -0300

    Adicionar módulos no admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from usuarios.models import *
 
 admin.site.register(PessoaFisica)
-
 admin.site.register(PessoaJuridica)
+admin.site.register(Endereco)
+admin.site.register(Acessibilidade)
