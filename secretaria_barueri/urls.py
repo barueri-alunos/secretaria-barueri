e2fc34157e212413122e239266afa44d4509739e
unknown <lilian.cascardo@outlook.com>
2020-01-30 15:33:57 -0300

    alterar forms Avaliacao_Secretaria e Form

--- secretaria_barueri/urls.py
+++ secretaria_barueri/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('vagas/', include('vaga.urls')),
     path('dados/', include('usuarios.urls'))
     path('avaliacao/', )
-
+    path('cadastro/pf', cadastrar_pessoa_fisica),
+    path('cadastro/pf/acessibilidade', acessibilidade_cadastro),
+    path('cadastro/pj', cadastrar_empresa),
+    path('admin/', admin.site.urls),
+    path('avaliacao/secretario', avaliacao_secretaria),
 ]
