c4ba40b4735770ddcbbedc22bb8d7c5aeea64998
glaucio <glauciocardoso@gmail.com>
2020-01-30 10:48:48 -0300

    Excluir atributos na class PessoaFisica

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 
 class PessoaFisica(User):
-<<<<<<< HEAD
-=======
+    class Meta:
+        verbose_name = 'Pessoa Física'
+
     nome = models.CharField(verbose_name = "Nome:", max_length=255)
     cpf = models.CharField(verbose_name = "CPF", max_length=14)
     generos = (
@@ -31,41 +32,8 @@ class PessoaFisica(User):
     )
     telefone = models.CharField(verbose_name ="Telefone", choices=telefones, max_length=13, default=True)
     telefone = models.CharField(verbose_name="Telefone", max_length=13)
-    # email = models.EmailField(verbose_name="E-mail")
     ativo = models.BooleanField(default=False)
-    criado_em = models.DateField(default=timezone.now, verbose_name = "Criado", max_length=255)
-    tipos_de_deficiencia=(
-        ("FISICA", "Fisica"),
-        ("CADERANTE","Cadderante"),
-        ("AUDITIVA","Auditiva"),
-        ("VISUAL","Visual"),
-        ("INTELECTUAL","Itelectual"),
-        ("MULTIPLA","Mutipla"),
-        ("TEA", "Tea"),
-        ("TRANSTORNO MENTAL","Trasntorno Mental"),
-        ("OUTOS","Outros"),
-    )
-    tipo_de_deficiencia = models.CharField(verbose_name="Restirões",choices=tipos_de_deficiencia, max_length=255, default=True)
-    outros = models.CharField(verbose_name="Outros", max_length=255, default=True)
-    cid = models.CharField(verbose_name="CID",max_length=150, )
-    recursos = (
-        ('sim', 'sim'),
-        ('nao', 'nao'),
-    )
-    recurso = models.CharField(verbose_name="Restrições",max_length=255, choices=recursos, default=True)
-    observacao_recurso = models.CharField(verbose_name="Observação do recurso", max_length = 255, default=True)
-    tecnologia_assistiva = models.BooleanField(verbose_name="Você precisa de Tecnologia Assistiva?",default=False)
-    tecnologia_assistiva = models.CharField(max_length=255)
-    transporte_mobilidade = models.BooleanField(verbose_name="Transporte",default=False)
-    transporte_mobilidade = models.CharField(max_length=255)
-    automovel_proprio = models.BooleanField(verbose_name="Automovel",default=False)
-    automovel_proprio = models.CharField(max_length=255)
-    recursos_financeiros = models.BooleanField(verbose_name="Financeiro", default=False)
-    recursos_financeiros = models.CharField(max_length=255)
-    
->>>>>>> 24ea8f7... Corrigir erros de escritas da models
-    class Meta:
-        verbose_name = 'Pessoa Física'
+    criado_em = models.DateField(default=timezone.now, verbose_name = "Criado", max_length=255)        
 
 class PessoaJuridica(User):
     nome_fantasia = models.CharField(max_length=255)
