3f3728c66843206d231ab3cd606f0193085f8daa
glaucio <glauciocardoso@gmail.com>
2020-01-31 10:49:49 -0300

    finalizar do crud da pessoa fisica aplicação: detalhar/atualizar/deletar

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,9 @@ from usuarios.forms import *
 from usuarios.models import *
 # Create your views here.
 
+def home(request):
+    return render(request, 'base.html')
+
 def cadastrar_empresa(request):
     form = PessoaJuridicaForm(request.POST or None)
     args = {
@@ -27,6 +30,35 @@ def cadastrar_pessoa_fisica(request):
         return render(request, 'cadastro.html', args)
     args = {'form':form}
     return render(request, 'cadastro.html', args)
+
+def detalhes_pessoa_fisica(request):
+    pf= PessoaFisica.objects.filter().all()
+    args = {'pessoafisica': pessoafisisca}
+    return render(request, 'detalhes.html', args)
+
+def atualizar_pessoa_fisica(request, id):
+    pf = pessoafisica.objects.get(pk = id)
+    form = PessoaFisicaForm(request.POST or None, instance = pf)
+
+    args = {'form': form}
+
+    if form.is_valid():
+        form.save()
+        args = {'msg': 'Cadastro atualizado com sucesso'}
+        return render(request, 'atualizarpf.html', args)
+
+def deletar_pessoa_fisica(request, id):
+    pf = pessoafisica.objects.get(pk = id)
+    form = PessoaFisicaForm(request.POST or None, instance = pf)
+
+    args = {'form': form}
+
+    if form.is_valid():
+        form.save()
+        args = {'msg': 'Cadastro deletaado com sucesso'}
+        return render(request, 'delitepf.html', args)
+
+
 
 def acessibilidade_cadastro(request): 
 
