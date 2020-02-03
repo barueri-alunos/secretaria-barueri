8330579a23a526e3bab87cbc637bb57b43f1abb9
unknown <lilian.cascardo@outlook.com>
2020-02-03 15:55:27 -0300

    finalizar atributos na models class curriculo

--- curriculo/models.py
+++ curriculo/models.py
@@ -1,10 +1,26 @@
 from django.db import models
-from usuarios.choices import escolaridades
+from usuarios.choices import *
 
 # Create your models here.
-class Formacao_academica(models.Model):
+class Competencia(models.Model):
     class Meta:
-        verbose_name = 'Formação acadêmica'
+        verbose_name = 'Competência'
     
+    objetivo = models.CharField(max_length=255, verbose_name='Objetivos')
+    qualificacao_profisssional = models.TextField(verbose_name='Qualificação profissional')
+    prentencao_salarial = models.DecimalField(verbose_name='Pretenção salarial')
     escolaridade = models.CharField(max_length=128, choices= escolaridades, verbose_name='Escolaridade')
+    qual_curso = models.CharField(max_length=100, verbose_name='Qual o curso')
+    instituicao_ensino_curso = models.CharField(max_length=255, verbose_name='Instituição que realizou o curso')
+    ano_inicio = models.DateField(verbose_name='Ano de inicio')
+    ano_termino = models.DateField(verbose_name='Ano de termino')
+    cursos_extras = models.CharField(max_length=255, verbose_name='Cursos extras')
+    nivel_de_leitura = models.CharField(max_length=100, choices=leitura, verbose_name='Nivel de leitura')
+    nivel_informatica = models.CharField(max_length=100, choices=informatica, verbose_name='Nivel de informatica')
+    lingua_estrangeira = models.BooleanField(verbose_name='Lingua Estrangeira')
+    lingua = models.CharField(null=True, blank=True)
+
+
+
+
     
--- usuarios/choices.py
+++ usuarios/choices.py
@@ -81,3 +81,33 @@ escolaridades = (
         ('PGCu', 'Pós Graduação Cursando'),
 
     )
+leitura = (
+    ('NA', 'Não alfabetizado'),
+    ('B', 'Básico (textos simples)'),
+    ('LEA', 'Leitura e escrita avaçada (sem dificuldades)'),
+)  
+informatica = (
+    ('Word', (
+      ('B', 'Básico'),
+      ('I', 'Intermediario'),
+      ('A', 'Avançado'),
+      ('NT', 'Não tenho')),),
+    ('Excel', (
+      ('B', 'Básico'),
+      ('I', 'Intermediario'),
+      ('A', 'Avançado'),
+      ('NT', 'Não tenho')),),
+    ('PowerPoint', (
+      ('B', 'Básico'),
+      ('I', 'Intermediario'),
+      ('A', 'Avançado'),
+      ('NT', 'Não tenho')),),
+    ('Internet', (
+      ('B', 'Básico'),
+      ('I', 'Intermediario'),
+      ('A', 'Avançado'),
+      ('NT', 'Não tenho')),),
+    
+) 
+
+
