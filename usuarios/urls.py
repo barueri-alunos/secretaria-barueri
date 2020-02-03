6bf9c4d618b2c57556fcdc036a57c4c7d867291d
glaucio <glauciocardoso@gmail.com>
2020-02-03 15:17:53 -0300

    Patronizar as end points da pf

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path ('cadastro/pf/atualizar/<int:id>',atualizar_cadastro),
     path ('cadastro/pf/deletar/int:id>',deletar_cadastro),
     path('cadastro/pf/acessibilidade', acessibilidade_cadastro),
-    path('pf/detalhes/<int:id>', detalhes_pessoa_fisica),
+    path('pf/detalhes/<int:id>',detalhes_pessoa_fisica),
     path('pf/editar/<int:id>', atualizar_pessoa_fisica),
     path('pf/remover/<int:id>', deletar_pessoa_fisica),
     path('pj/detalhes/<int:id>', detalhar_empresa),
