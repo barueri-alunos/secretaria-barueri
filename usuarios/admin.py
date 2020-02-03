5507201f3443ad892f6351297b9173dd7e0b4ca7
diandra <di.andrade90@gmail.com>
2020-02-03 18:09:55 -0300

    Incluir método para habilitar o export em csv

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from usuarios.models import *
+from usuarios.export import ExportCsvMixin
 
 
-class PFAdmin(admin.ModelAdmin):
+class PFAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('nome', 'cpf', 'celular', 'genero', 'ativo')
     ordering = ('-criado_em',)
     search_fields = ['nome', 'cpf', 'celular', 'genero', 'ativo']
+    actions = ['export_as_csv']
+    
 
-
-class PJAdmin(admin.ModelAdmin):
+class PJAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('razao_social', 'cnpj', 'contato_empresa')
     search_fields = ['razao_social', 'cnpj', 'contato_empresa']
+    actions = ['export_as_csv']
     
     
 class EnderecoAdmin(admin.ModelAdmin):
