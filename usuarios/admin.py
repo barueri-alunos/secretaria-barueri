3841d120dd2e512dadd9ed72acf11eb43dd6f7fb
diandra <di.andrade90@gmail.com>
2020-02-03 17:48:27 -0300

    Adicionar colunas dentro do django admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from usuarios.models import *
 
-admin.site.register(PessoaFisica)
-admin.site.register(PessoaJuridica)
-admin.site.register(Endereco)
+
+class PFAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'celular', 'genero', 'ativo')
+    ordering = ('-criado_em',)
+    search_fields = ['nome', 'cpf', 'celular', 'genero', 'ativo']
+
+
+class PJAdmin(admin.ModelAdmin):
+    list_display = ('razao_social', 'cnpj', 'contato_empresa')
+    search_fields = ['razao_social', 'cnpj', 'contato_empresa']
+    
+    
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'logradouro', 'numero', 'cidade', 'cep')
+    search_fields = ['usuario', 'logradouro', 'numero', 'cidade', 'cep']
+    
+    
+admin.site.register(PessoaFisica, PFAdmin)
+admin.site.register(PessoaJuridica, PJAdmin)
+admin.site.register(Endereco, EnderecoAdmin)
 admin.site.register(Acessibilidade)
--- vaga/admin.py
+++ vaga/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+
+class VagasAdmin(admin.ModelAdmin):
+    list_display = ('titulo_vaga', 'area_vaga', 'empresa', 'data_abertura', 'ativo')
+    search_fields = ['titulo_vaga', 'area_vaga', 'empresa', 'data_abertura', 'ativo']
+    
 # Register your models here.
 admin.site.register(Vaga)
