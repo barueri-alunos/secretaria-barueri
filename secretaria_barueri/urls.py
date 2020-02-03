770a8aa86d6a87782e24d3f72e17b1ade7a3382b
Charles <vinnesousa@gmail.com>
2020-02-03 14:52:05 -0300

    Corrigir o arquivo urls.py do projeto

--- secretaria_barueri/urls.py
+++ secretaria_barueri/urls.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vagas/', include('vaga.urls')),
-    path('dados/', include('usuarios.urls'))
-    path('avaliacao/', )
-    path('cadastro/pf', cadastrar_pessoa_fisica),
-    path('cadastro/pf/acessibilidade', acessibilidade_cadastro),
-    path('cadastro/pj', cadastrar_empresa),
-    path('admin/', admin.site.urls),
-    path('avaliacao/secretaria', avaliacao_secretaria),
-    path('cadastro/', include(usuarios.urls))
+    path('dados/', include('usuarios.urls')),
 ]
