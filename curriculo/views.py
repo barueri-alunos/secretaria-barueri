f9b27600eb78e65d17310e4119d5972ce8b1216d
unknown <lilian.cascardo@outlook.com>
2020-02-05 09:51:54 -0300

    criar id no app curriculo

--- curriculo/views.py
+++ curriculo/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from curriculo.forms import *
-from curriculo.models import *
+from usuarios.models import PessoaFisica
+from .models import *
 
 # Create your views here.
 def cadastrar_competencia(request, id):
-    pff = PessoaFisica.objects.get(pk=id)
-    form = CompetenciaForm(request.POST or None, instance=pff)
+    pessoa_fisica = PessoaFisica.objects.get(pk=id)
+    form = CompetenciaForm(request.POST or None, instance=pessoa_fisica)
     args ={
-            'pff':pff,
+            'form':form
         }
     if form.is_valid():
         form.save()
-        args['msg'] = 'Curriculo cadatrado com sucesso!'
-        return render(request,'competencia.html',args)
-        curricu = form.save(commit=False)
-        seu_curricu = PessoaFisica
-        curricu.save()
-    return  render(request, 'competencia.html',args)
+        args = {
+            'msg' :'Curriculo cadatrado com sucesso!'
+        }
+        return render(request,'cadastrar_competencia.html',args)
+    return  render(request, 'cadastrar_competencia.html',args)
 
 
 
