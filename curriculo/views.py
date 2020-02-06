b0c0a4972452ef0ea7ec20143fada39e51dbbc58
diandra <di.andrade90@gmail.com>
2020-02-06 15:11:58 -0300

    Correção da parte de cv

--- curriculo/views.py
+++ curriculo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from curriculo.forms import *
 from curriculo.models import *
+from usuarios.models import PessoaFisica
 
 # Create your views here.
 def cadastrar_competencia(request):
@@ -14,3 +15,28 @@ def cadastrar_competencia(request):
         return render(request,'competencia.html',args)
     return  render(request, 'competencia.html',args)   
         
+        
+def detalhes_competencia(request, id):
+    competencia = PessoaFisica.objects.get(pk=id)
+    form = CompetenciaForm(request.POST or None)
+    args = {
+        'form':form,
+        }
+    return render(request, 'detalhes_competencia.html', args)
+
+def atualizar_competencia(request, id):
+    competencia = PessoaFisica.objects.get( pk=id)
+    form = CompetenciaForm(request.POST or None)
+    args = {
+        'form':form,
+        }
+    return render(request, 'atualizar_competencia.html', args)
+
+
+def deletar_competencia(request, id):
+    competencia = PessoaFisica.objects.get(pk=id)
+    form = CompetenciaForm(request.POST or None)
+    args = {
+        'form':form,
+    }
+    return render(request, 'deletar_competencia.html', args)
