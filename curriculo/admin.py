36cdd5a51c783b26dd9034576652c0be78a9cd0b
diandra <di.andrade90@gmail.com>
2020-02-07 08:55:22 -0300

    Corrigir cv

--- curriculo/admin.py
+++ curriculo/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from curriculo.models import Competencia
 
-# Register your models here.
+admin.site.register(Competencia)
--- curriculo/views.py
+++ curriculo/views.py
@@ -11,7 +11,9 @@ def cadastrar_competencia(request):
         }
     if form.is_valid():
         form.save()
-        args['msg'] = 'Curriculo cadatrado com sucesso!'
+        args = {
+            'msg': 'Curriculo cadastrado com sucesso'
+        }
         return render(request,'competencia.html',args)
     return  render(request, 'competencia.html',args)   
         
